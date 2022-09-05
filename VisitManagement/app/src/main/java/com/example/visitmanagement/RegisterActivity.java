43c3f568bbc83b99338de9e2a6e9609cd5ba9a32
Seunghye Hwang <ferrydraw@naver.com>
2022-09-05 14:39:10 +0900

    register수정

--- VisitManagement/app/src/main/java/com/example/visitmanagement/RegisterActivity.java
+++ VisitManagement/app/src/main/java/com/example/visitmanagement/RegisterActivity.java
@@ -18,7 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText et_id, et_pw, et_pwcheck, et_name,et_phone, et_belong;
+    private EditText et_id, et_pw, /*et_pwcheck,*/ et_name,et_phone, et_belong;
     private Button btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +28,20 @@ public class RegisterActivity extends AppCompatActivity {
         //값 찾아주기
         et_id=findViewById(R.id.et_id);
         et_pw=findViewById(R.id.et_pw);
-        et_pwcheck=findViewById(R.id.et_pwcheck);
+        //et_pwcheck=findViewById(R.id.et_pwcheck);
         et_name=findViewById(R.id.et_name);
         et_phone=findViewById(R.id.et_phone);
         et_belong=findViewById(R.id.et_belong);
 
         //회원가입 버튼 클릭 시 수행
         btn_register= findViewById(R.id.btn_register);
-        btn_register.setOnClickListener(view -> {
+        /*btn_register.setOnClickListener(view -> {
             //editText에 입력되어 있는 값 get
             String userID=et_id.getText().toString();
             String userPass=et_pw.getText().toString();
-            String userPassCheck=et_pwcheck.getText().toString();
+            //String userPassCheck=et_pwcheck.getText().toString();
             String userName=et_name.getText().toString();
-            int userPhone=Integer.parseInt(et_phone.getText().toString());
+            String userPhone=et_phone.getText().toString();
             String userBelong=et_belong.getText().toString();
 
             Intent intent=new Intent(RegisterActivity.this, Mainpage.class);
@@ -62,12 +62,44 @@ public class RegisterActivity extends AppCompatActivity {
                 }catch (JSONException e){
                     e.printStackTrace();
                 }
-            };
-            // 서버로 Volley를 이용해서 요청을 함.
-            RegisterRequest registerRequest = new RegisterRequest(userID,userPass,userPassCheck,userName,userPhone,userBelong, responseListener);
-            RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-            queue.add(registerRequest);
-        });
+            };*/
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ID = et_id.getText().toString();
+                String Password = et_pw.getText().toString();
+                //String userPassCheck=et_pwcheck.getText().toString();
+                String Name = et_name.getText().toString();
+                String Phone_Num = et_phone.getText().toString();
+                String Belonging = et_belong.getText().toString();
 
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            //회원가입 성공시
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원가입 실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                // 서버로 Volley를 이용해서 요청을 함.
+                RegisterRequest registerRequest = new RegisterRequest(ID, Password,/*userPassCheck,*/Name, Phone_Num, Belonging, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
