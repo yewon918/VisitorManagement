cf513d8464b20bad5fd3673d8dceb1ba541911fb
Seunghye Hwang <ferrydraw@naver.com>
2022-09-19 16:19:10 +0900

    수정

--- VisitManagement/app/src/main/java/com/example/visitmanagement/LoginRequest.java
+++ VisitManagement/app/src/main/java/com/example/visitmanagement/LoginRequest.java
@@ -7,15 +7,15 @@ import java.util.Map;
 
 public class LoginRequest extends StringRequest
 {  // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://localhost:8080/login.php";
+    final static private String URL = "http://ferrydraw.dothome.co.kr/login.php";
     private Map<String, String> map;
 
-    public LoginRequest(String userID, String userPass, Response.Listener<String> listener) {
+    public LoginRequest(String ID, String Password, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
-        map.put("userID",userID);
-        map.put("userPass", userPass);
+        map.put("ID",ID);
+        map.put("Password", Password);
 
     }
 
--- VisitManagement/app/src/main/java/com/example/visitmanagement/register.php
+++ VisitManagement/app/src/main/java/com/example/visitmanagement/register.php
@@ -1,17 +1,16 @@
 <?php 
-    header('Context-Type: application/json');
-
-    $con = mysqli_connect("localhost", "root", "root", "rtw");
+    $con = mysqli_connect("localhost", "ferrydraw", "hsh0729!", "ferrydraw");
     mysqli_query($con,'SET NAMES utf8');
 
     $ID = isset($_POST["ID"]) ? $_POST["ID"] : "";
     $Password = isset($_POST["Password"]) ? $_POST["Password"] : "";
     $Name = isset($_POST["Name"]) ? $_POST["Name"] : "";
     $Phone_Num = isset($_POST["Phone_Num"]) ? $_POST["Phone_Num"] : "";
+    $profileimage = isset($_POST["profileimage"]) ? $_POST["profileimage"] : "";
     $Belonging = isset($_POST["Belonging"]) ? $_POST["Belonging"] : "";
 
-    $statement = mysqli_prepare($con, "INSERT INTO signeduser_table VALUES (?,?,?,?,?)");
-    mysqli_stmt_bind_param($statement, "sssss", $ID, $Password, $Name, $Phone_Num, $Belonging);
+    $statement = mysqli_prepare($con, "INSERT INTO signeduser_table VALUES (?,?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "ssssss", $ID, $Password, $Name, $Phone_Num, $profileimage, $Belonging);
     mysqli_stmt_execute($statement);
 
 
